1128429651af93f224bb57e8e3597745f244cf20
tasmim846 <trimvy@gmail.com>
2025-05-15 22:04:11 +0600

    Remove node_modules from Git tracking

---
+++ backend/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app=  express();
+
+app.get("/", (req,res) => {
+    res.send("Server is ready");
+});
+
+ app.listen(8080, () => {
+    console.log("Server started at http://localhost:8080");
+ });
